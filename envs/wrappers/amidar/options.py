198ddea353d3223ca025cee94281f098f9a089a9
Kate Avery <kteavery@users.noreply.github.com>
2022-08-18 13:25:15 -0400

    tilepoint bug

--- envs/wrappers/amidar/options.py
+++ envs/wrappers/amidar/options.py
@@ -2,14 +2,20 @@ from ctoybox import Toybox, Input
 import toybox.interventions.amidar as amidar
 from toybox.interventions.amidar import AmidarIntervention, Amidar
 
+from abc import ABC, abstractmethod
+
 possible_options = [(25, 217)]
 
 def tilepoint_to_junctionpoint(x, y):
     return (31-y)*32 + x
 
+def json_to_tilepoint(x, y):
+    SCALE = 16
+    TILE_SIZE = (4,5)
+    return x/(TILE_SIZE[0]*16), y/(TILE_SIZE[1]*16)
+
 class FillOption:
     def __init__(self):
-        self.tb = Toybox('amidar')
         self.start = -1 
         self.destination = -1
     
@@ -20,33 +26,31 @@ class FillOption:
         * TODO Destination must be unpainted.
         * TODO All enemies must be 7+ tiles away. 
         """
-        with AmidarIntervention(self.tb) as intervention:
-            position = intervention.worldpoint_to_tilepoint(intervention.game.player.position)
-            # print(intervention.game.player.position)
-            # print(position)
-            position_junction = tilepoint_to_junctionpoint(position.tx, position.ty)
-            print("Position:")
-            print(position_junction)
-            for possible in possible_options:
-                if possible[0] == position_junction:
-                    self.start = possible[0]
-                    self.destination = possible[1]
-                    return (self.start, self.destination)
-            return None
-
+        state = env.toybox.state_to_json()
+        json_position = state["player"]["position"]
+        x, y = json_to_tilepoint(json_position["x"], json_position["y"])
+        position_junction = tilepoint_to_junctionpoint(x, y)
+        
+        print("Position:")
+        print(position_junction)
+        for possible in possible_options:
+            if possible[0] == position_junction:
+                self.start = possible[0]
+                self.destination = possible[1]
+                return (self.start, self.destination)
+        return None
 
     def get_action(self):
         if self.start < self.destination: 
             if self.destination-self.start <= 31:
-                return RIGHT
+                return 3 # RIGHT
             else:
-                return DOWN
+                return 5 # DOWN
         else:
             if self.start-self.destination <= 31:
-                return LEFT
+                return 4 # LEFT
             else:
-                return UP
-
+                return 2 # UP
 
     def terminate(self):
         """
@@ -59,4 +63,21 @@ class FillOption:
         if position_junction == self.destination:
             return True
         return False
+
+
+class AbstractOption: 
+    def __init__(self):
+        pass
+
+    @abstactmethod
+    def initiate(self):
+        pass
+
+    @abstactmethod
+    def get_action(self):
+        pass
     
+    @abstactmethod
+    def terminate(self):
+        pass
+
