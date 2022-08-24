68e59c4ce613dca560d44a7a536acb75a530f02f
Kate Avery <kteavery@users.noreply.github.com>
2022-08-24 15:26:09 -0400

    change params to original

--- scripts/option_critic.sh
+++ scripts/option_critic.sh
@@ -3,8 +3,8 @@
 #SBATCH -p gpu  # Partition
 #SBATCH -G 1  # Number of GPUs
 #SBATCH -t 10:00:00  # Job time limit
-#SBATCH -o storage/logs/option-critic-%j.out  # %j = job ID
-#SBATCH -e storage/logs/option-critic-%j.err 
+#SBATCH -o out/option-critic-%j.out  # %j = job ID
+#SBATCH -e out/option-critic-%j.err 
 
 cd option_critic
 python -m main --env="AmidarNoFrameskip-v4"
